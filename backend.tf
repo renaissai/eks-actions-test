ed6b46099299fef788d591a5675af1a92a957742
GpleytonR <gerardo@renaiss.ai>
2024-02-07 14:50:14 -0300

    add ec2 test2

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "eks-terraform-state-aiforce"
-    key    = "/actions-test/ekstest.tfstate"
+    key    = "actions-test/ekstest.tfstate"
     region = "eu-west-1"
   }
 }
